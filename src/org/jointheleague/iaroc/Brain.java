0b6819aac87b4f0be446212a56a942bed6f3156d
codeavengers <eshaank@gmail.com>
2016-05-22 15:13:48 -0700

    YAYAYYYYAYA
    ∆∆∆∆∆∆ illuminates with u

--- src/org/jointheleague/iaroc/Brain.java
+++ src/org/jointheleague/iaroc/Brain.java
@@ -1,14 +1,19 @@
 package org.jointheleague.iaroc;
 
-import ioio.lib.api.IOIO;
-import ioio.lib.api.exception.ConnectionLostException;
+import android.os.SystemClock;
+
+import org.jointheleague.iaroc.sensors.UltraSonicSensors;
 import org.wintrisstech.irobot.ioio.IRobotCreateAdapter;
 import org.wintrisstech.irobot.ioio.IRobotCreateInterface;
-import org.jointheleague.iaroc.sensors.UltraSonicSensors;
+
+import ioio.lib.api.IOIO;
+import ioio.lib.api.exception.ConnectionLostException;
 
 public class Brain extends IRobotCreateAdapter {
     private final Dashboard dashboard;
     public UltraSonicSensors sonar;
+    int distance = 4000;
+    long startTime = System.currentTimeMillis();
 
     public Brain(IOIO ioio, IRobotCreateInterface create, Dashboard dashboard)
             throws ConnectionLostException {
@@ -19,13 +24,26 @@ public class Brain extends IRobotCreateAdapter {
 
     /* This method is executed when the robot first starts up. */
     public void initialize() throws ConnectionLostException {
-        dashboard.log("Hello! I'm a Clever Robot!");
-        //what would you like me to do, Clever Human?
-
-
 
 
     }
+
     /* This method is called repeatedly. */
-    public void loop() throws ConnectionLostException {}
+    public void loop() throws ConnectionLostException {
+        dashboard.log((System.currentTimeMillis() - startTime) / 1000f + " Seconds");
+        readSensors(6);
+        distance = getWallSignal();
+        if (distance < 10) {
+            driveDirect(75, 400);
+        } else {
+            driveDirect(400, 75);
+        }
+        readSensors(SENSORS_BUMPS_AND_WHEEL_DROPS);
+        if (isBumpLeft()) {
+            driveDirect(-500, -500);
+            SystemClock.sleep(100);
+            driveDirect(400, -400);
+            SystemClock.sleep(500);
+        }
+    }
 }
